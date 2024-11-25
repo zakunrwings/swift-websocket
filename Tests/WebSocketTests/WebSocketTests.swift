ce5268e5dd909ce00c05daef6743fd9d78ae4291
zakunrwings <4297525+zakunrwings@users.noreply.github.com>
2024-11-25 18:02:16 -0300

    Use XCTest

--- Tests/WebSocketTests/WebSocketTests.swift
+++ Tests/WebSocketTests/WebSocketTests.swift
@@ -1,7 +1,9 @@
-import Testing
+import XCTest
 
 @testable import WebSocket
 
-@Test func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class WebSocketTests: XCTestCase {
+  func testExample() {
+    // Write your test here and use APIs like `XCTAssertEqual` to check expected conditions.
+  }
 }
